2deecc42656863bb88e5d0cf5841fe8e061c4722
Vanson123 <vans72092@gmail.com>
2024-01-08 22:11:42 +0700

    feat: finish footer desktop

--- app.js
+++ app.js
@@ -23,103 +23,97 @@ nav.forEach(function (navElement, i) {
   })
 });
 
-// nav_bar.addEventListener("mouseenter", function(){
-//   timeoutId = setTimeout(function(){
-//     nav.forEach(function (navElement, i) {
-//       navElement.addEventListener("mouseenter", function(e) {
-//           parent[i].classList.add("d-block");
-//         // setTimeout(function(){
-//         //   clearTimeout(timeoutId);
-//         // }, 100)
-//       });
-//       navElement.addEventListener("mouseleave", function (e) {
-//           parent[i].classList.remove("d-block");
-//       });
-//       hihi[i].addEventListener("mouseenter", function (e) {
-//           parent[i].classList.remove("d-block");
-//       })
-//     });
-//   }, 200)
-// })
 
-// nav_bar.addEventListener("mouseout", function(){
-//     clearTimeout(timeoutId);
-// })
+// var wrapper = document.querySelector("#wrapper");
+// var header = document.querySelector(".header");
+// var content = document.querySelector("#content");
+// var lastScrollTop = 0;
 
-
-var dropdown_main = document.querySelectorAll(".dropdown-main")
-
-// window.addEventListener("scroll", function () {
-//   if (window.scrollY > 0) {
-//     dropdown_main.forEach(function (dropdownElement, i) {
-//       // dropdownElement.classList.add("fixed-top");
-//     });
-//     document.querySelector(".wrapper").classList.add("hide-navbar");
-//     // document.querySelector(".wrapper").classList.add("position-absolute");
-//   } else {
-//     dropdown_main.forEach(function (dropdownElement, i) {
-//       // dropdownElement.classList.remove("fixed-top");
-//     });    document.querySelector(".wrapper").classList.remove("hide-navbar");
-//     // document.querySelector(".wrapper").classList.remove("position-absolute");
-//     // remove padding top from body
-//   }
-// });
-
-window.addEventListener("scroll", function () {
-  var wrapper = document.querySelector(".wrapper");
-
-  if (window.scrollY > 0) {
-    dropdown_main.forEach(function (dropdownElement, i) {
-      // dropdownElement.classList.toggle("fixed-top", true);
-    });
-    wrapper.classList.add("hide-navbar");
-    // wrapper.classList.add("position-absolute");
-  } else {
-    dropdown_main.forEach(function (dropdownElement, i) {
-      // dropdownElement.classList.toggle("fixed-top", false);
-    });
-    wrapper.classList.remove("hide-navbar");
-    // wrapper.classList.remove("position-absolute");
-  }
-});
-
-
-var wrapper = document.querySelector("#wrapper");
-var header = document.querySelector("#header");
-// var lastScrollTop;
 // // navbar = document.getElementById('navbar');
 // window.addEventListener('scroll',function(){
 // var scrollTop = wrapper.pageYOffset || document.documentElement.scrollTop;
 // if(scrollTop > lastScrollTop){
-// header.style.top='0';
+//   // if(content.top <= '150px'){
+//     header.style.top='-152px';
+//   // }
 // }
 // else{
-// header.style.top='-80px';
+// header.style.top='0';
 // }
 // lastScrollTop = scrollTop;
 // });
 
-// var prevScrollpos = wrapper.pageYOffset;
-// wrapper.onscroll = function() {
-// var currentScrollPos = wrapper.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     header.classList.add("hide-navbar");
+// var wrapper = document.querySelector("#wrapper");
+// var header = document.querySelector(".header");
+// var content = document.querySelector("#content");
+// var lastScrollTop = 0;
+
+// window.addEventListener('scroll', function () {
+//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+//   // Get the top position of the content element
+//   var contentTop = content.getBoundingClientRect().top;
+
+//   if (scrollTop > lastScrollTop) {
+//     if (contentTop <= 50) {
+//       header.style.top = '-152px';
+//     }
 //   } else {
-//     header.classList.remove("hide-navbar");
+//     header.style.top = '0';
 //   }
-//   prevScrollpos = currentScrollPos;
-// }
+
+//   lastScrollTop = scrollTop;
+// });
+
+// 
+
+// hide .header when scrolling
+var wrapper = document.querySelector("#wrapper");
+var header = document.querySelector(".header");
+var content = document.querySelector("#content");
+var menu_control = document.querySelector(".header-search");
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', function () {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  // Get the top position of the content element
+  var contentTop = content.getBoundingClientRect().top;
+
+  // Check if menu_control is set to display: none
+  var isMenuHidden = getComputedStyle(menu_control).display === "none";
+
+  if (scrollTop > lastScrollTop) {
+    if (contentTop <= 100 && isMenuHidden) {
+      header.style.top = '-152px';
+    }
+  } else {
+    header.style.top = '0';
+  }
+
+  lastScrollTop = scrollTop;
+});
+
+
 
 var form_menu = document.querySelector(".form-search");
 var menu_control = document.querySelector(".header-search");
 var exit_menu = document.querySelector(".btn-close");
 var pseudo_search = document.querySelector(".pseudo-search");
 
-form_menu.addEventListener("click", function() {menu_control.classList.add("d-block")})
+form_menu.addEventListener("click", function() {
+  menu_control.classList.add("d-block")
+  // header.classList.add("fixed-top")
+})
 exit_menu.addEventListener("click", function() {
   menu_control.classList.remove("d-block")
+  // header.classList.remove("fixed-top")
+
 })
-pseudo_search.addEventListener("click", function() {menu_control.classList.remove("d-block")})
+pseudo_search.addEventListener("click", function() {
+  menu_control.classList.remove("d-block")
+  // header.classList.remove("fixed-top")
+})
 
 // end navbar
 // var btn1 = document.querySelector(".product_tab button:first-child");
@@ -153,14 +147,3 @@ option.forEach(function(element, i) {
 })
 
 })
-// function changeColor(element) {
-//   // Change color of the focused box
-//   element.style.border = '1px solid #000'
-
-//   // Reset color of the remaining boxes
-//   document.querySelectorAll('.product-container-option').forEach(box => {
-//     if (box !== element) {
-//       box.style.border = ''
-//     }
-//   });
-// }
